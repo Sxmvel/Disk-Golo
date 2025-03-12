3a5919a4a20b697200daea3dc35133748cb5e919
Samuel Resende <samuelres444@gmail.com>
2025-03-12 20:57:29 -0300

    descrição do main

--- src/diskgolo/DiskGolo.java
+++ src/diskgolo/DiskGolo.java
@@ -3,6 +3,8 @@ package diskgolo;
 public class DiskGolo {
 
     public static void main(String[] args) {
+        // Trabalho Final da disciplina de programação orientada a objetos.
+        
         Bar diskGolo = new Bar(); // Iniciamos nosso Bar.
 
         diskGolo.menuDoBar();
